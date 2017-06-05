0223e09e1392cb25ecccace582e137a3bc3ab20b
Akash Garg <akash.garg@sap.com>
2017-06-05 22:58:19 +0530

    Host Listener and Host Binding for Element Interaction
    Change-Id: Ia2435a6f0b9fea05a5b3597f8ff04032da5dfb5d
    Signed-off-by: Akash Garg <akash.garg@sap.com>

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[dirHighlight]'
@@ -6,8 +6,18 @@ import { Directive, ElementRef, Renderer } from '@angular/core';
 export class HighlightDirective {
 
   constructor(private elementRef: ElementRef, private renderer: Renderer ) {   //this will automatically create a private property this.elementaRef
-     //   this.elementRef.nativeElement.style.backgroundColor = 'green';
-      this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green');
-  }    
-      
+     //   this.elementRef.nativeElement.style.backgroundColor = 'green';  //One way to create some properties in the directive
+  //    this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green');   //This is the better way of doing the same
+  }  
+      @HostListener('mouseenter') mouseover(){
+        this.backgroundColor = 'green';  
+      };
+      @HostListener('mouseleave') mouseleave(){
+        this.backgroundColor = 'white';  
+      };
+      @HostBinding('style.backgroundColor') get setColor() {  //This is the way to write in typescript get means this is 
+      //read only property, property name is setColor which internally angualar2 will change from function to property
+        return this.backgroundColor;    
+      }
+      private backgroundColor = 'yellow';
 }
